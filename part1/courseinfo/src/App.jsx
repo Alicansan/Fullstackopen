334fea677530a815483aac2c1808c73e6e64c4b4
Alican Şan <alicansan2606@gmail.com>
2024-01-31 03:29:29 +0300

    update

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,25 +3,23 @@ import Content from './Components/Content'
 import Total from './Components/Total'
 import './App.css'
 
-
 const App = () => {
   const course = 'Half Stack application development'
 
   const parts = [
     {
       name: 'Fundamentals of React',
-      exercises: 10
+      exercises: 10,
     },
     {
       name: 'Using props to pass data',
-      exercises: 7
+      exercises: 7,
     },
     {
       name: 'State of a component',
-      exercises: 14
-    }
+      exercises: 14,
+    },
   ]
-
 
   return (
     <div>
@@ -29,12 +27,11 @@ const App = () => {
       <Content part={parts[0].name} excercises={parts[0].exercises} />
       <Content part={parts[1].name} excercises={parts[1].exercises} />
       <Content part={parts[2].name} excercises={parts[2].exercises} />
-      
 
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
-
-      
-
+      <Total
+        total={parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      />
     </div>
   )
 }
+export default App
