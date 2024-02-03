2f79aea8243cd8475f9ed9be5015d179ad384cb0
Alican Şan <alicansan2606@gmail.com>
2024-02-03 20:48:34 +0300

    Finish
    end of  Exercises 1.6.-1.14.

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import anecdotes from './components/anecdotes'
 
-//const getRnd =  ()=> Math.floor((Math.random() * 8))
+
 
 const App = ()=>{
   const [selected, setSelected]= useState(0)
+  //initialize with an array and give every index 0 via map method
   const [votePoint,setVotePoint] = useState(anecdotes.map(() => 0))
-
+  console.log(votePoint)
   const rndSelect =()=>{
     setSelected(()=> Math.floor((Math.random() * anecdotes.length)))
   }
@@ -17,14 +18,18 @@ const App = ()=>{
   }
   const mostVoted = Math.max(...votePoint)
   const index = votePoint.indexOf(mostVoted)
-  console.log(votePoint)
+
   return (
+    
     <div>
       <h3>Anecdote of the Day</h3>
+      
       <p> {anecdotes[selected].anecdote} </p>
       <p>has {votePoint[selected]} votes</p>
+
       <button onClick={rndSelect}>Next</button>
       <button onClick={() => voteBtn(selected)}>Vote</button>
+
       <p>Most voted anecdote is:</p>
       <p>{anecdotes[index].anecdote}</p>
       
